06adf5801492e02372dca9fef1c13ae4ce708201
Colon <ocolon@tfanet.org>
2019-05-08 09:10:27 -0400

    Updates comments to clarify placeholder method can be changed.

--- src/main/java/org/tfa/test/matching/Matcher.java
+++ src/main/java/org/tfa/test/matching/Matcher.java
@@ -3,7 +3,7 @@ package org.tfa.test.matching;
 public class Matcher {
 
     /*
-     * This method will be built out at past of the coding exercise
+     * This is the placeholder method to be built out as part of the coding exercise
      */
     public String getBestMatch() {
 
